88697cbe32c2673091ed2ab6c9b67c5e4b76c076
mylyf <mylyftest@gmail.com>
2023-04-01 20:22:50 +0530

    new

---
+++ src/test/java/org/example/nexttime/window.java
@@ -0,0 +1,39 @@
+package org.example.nexttime;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import javax.xml.xpath.XPath;
+
+public class window {
+    WebDriver driver;
+    public  window(WebDriver driver){
+        this.driver =driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//li[@id='menuform:m_window']") private WebElement windowbutton;
+
+    @FindBy(xpath = "//li[@id='menuform:j_idt38']") private WebElement browser;
+
+    @FindBy(xpath = "//button[@id='j_idt88:new']") private  WebElement firstframe;
+
+    public void windows(){
+        Actions act= new Actions(driver);
+        act.moveToElement(browser).click().perform();
+       act.moveToElement(windowbutton).click().perform();
+    }
+
+    public  void windowframe(){
+        String parentwindow = driver.getWindowHandle();
+        firstframe.click();
+        System.out.println(driver.getWindowHandle());
+        driver.switchTo().window(parentwindow);
+
+
+    }
+
+}
